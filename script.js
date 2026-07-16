11a58909de0c7d0f9dd0017c79e173f2366dd70d
kevin <kevin.dona00@gmail.com>
2026-07-16 10:30:11 +0200

    caricamento pulito

---
+++ script.js
@@ -0,0 +1,8 @@
+  let num1 = Number(prompt( "insert a number" ));
+         let num2 = Number(prompt( "insert a number" ));
+         let sum = num1 + num2;
+         let sottration = num1 - num2;
+         let multiplication = num1 * num2;
+         let division = num1 / num2;
+         let power = num1 ** num2;
+         console.log(sum,sottration,multiplication,division,power)
